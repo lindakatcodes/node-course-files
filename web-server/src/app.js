b21ba2145c700d4e8b11b41206cf1c53425bb250
Lindakat <lindakt16@gmail.com>
2019-04-05 21:42:15 -0500

    Started using Handlebars to create html templates and integrate them into express

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -6,8 +6,32 @@ const app = express();
 //__dirname will give us the path to our current directory; join then lets us write a string to go up a level and add our public folder
 const publicDirPath = path.join(__dirname, '../public');
 
+// using .set lets us set a var in express
+app.set('view engine', 'hbs');
+
 // customize server - in this instance, set a static path to our html files
 app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    // use render to show a view - first arg is name of template file, second is object with vars to use
+    res.render('index', {
+        title: 'Super Cool Weather App'
+    });
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About the Author',
+        name: 'Kat'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: 'Need some help? Let us know!'
+    })
+})
 
 // app.get is how you create routes - first arg is the name of the route, second is the action to take when someone reaches that route
 app.get('/weather', (req, res) => {
