9681d170bc99ea0a27fcedb84edc51da152281a4
Lindakat <lindakt16@gmail.com>
2019-04-06 21:54:30 -0500

    modified views folder, created partials, freshened up general css styles

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,21 +1,29 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 // express is actually a function in itself, so to set it onto an app we can run, we just call express
 const app = express();
+
 //__dirname will give us the path to our current directory; join then lets us write a string to go up a level and add our public folder
 const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 // using .set lets us set a var in express
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 // customize server - in this instance, set a static path to our html files
 app.use(express.static(publicDirPath))
 
+// app.get is how you create routes - first arg is the name of the route, second is the action to take when someone reaches that route
 app.get('', (req, res) => {
     // use render to show a view - first arg is name of template file, second is object with vars to use
     res.render('index', {
-        title: 'Super Cool Weather App'
+        title: 'Super Cool Weather App',
+        name: 'Kat'
     });
 })
 
@@ -29,17 +37,34 @@ app.get('/about', (req, res) => {
 app.get('/help', (req, res) => {
     res.render('help', {
         title: 'Help Page',
-        message: 'Need some help? Let us know!'
+        message: 'Need some help? Let us know!',
+        name: 'Kat'
     })
 })
 
-// app.get is how you create routes - first arg is the name of the route, second is the action to take when someone reaches that route
 app.get('/weather', (req, res) => {
     // we use .send when we want to send something to the browser
     res.send({
         forecast: 'cloudy',
         location: 'Idaho'
     });
+})
+
+app.get('/help/*', (req, res) => {
+    res.render('404', {
+        error: 'Help article not found.',
+        name: 'Kat',
+        title: 'Oh no!'
+    })
+})
+
+// * - wildcard character, match anything not explicitly defined
+app.get('*', (req, res) => {
+    res.render('404', {
+        error: 'Page not found.',
+        name: 'Kat',
+        title: 'Oh no!'
+    })
 })
 
 // to start the server, we listen on whatever port we set
