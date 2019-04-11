c5be031b2ee8b7f600bce9b19f1275a62eff66cb
Lindakat <lindakt16@gmail.com>
2019-04-10 23:22:57 -0500

    Finished functionality!

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -2,19 +2,24 @@ console.log('Client side JS loaded')
 
 const weatherForm = document.querySelector('form');
 const searchText = document.querySelector('input');
+const firstp = document.querySelector('#firstP');
+const secondp = document.querySelector('#secondP');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const location = searchText.value;
 
+    firstp.textContent = 'Loading...';
+    secondp.textContent = '';
+
     fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error);
+            firstp.textContent = data.error;
         } else {
-            console.log(data.location);
-            console.log(data.forecast);
+            firstp.textContent = data.location;
+            secondp.textContent = data.forecast;
         }
     })
 })
