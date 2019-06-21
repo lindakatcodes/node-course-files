fe8e4e34d88fb831f451cc5a61f34ce4cea86e14
Lindakat <lindakt16@gmail.com>
2019-06-20 23:50:27 -0500

    Users can log out, learned to hide private data, added authentication for users, tied tasks to specific users and handled auth for those

--- task-manager/src/models/user.js
+++ task-manager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const Task = require('./task')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -49,6 +50,22 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
+userSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    delete userObject.password;
+    delete userObject.tokens;
+
+    return userObject;
+}
+
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
 
@@ -56,7 +73,7 @@ userSchema.methods.generateAuthToken = async function () {
     
     user.tokens = user.tokens.concat({ token })
     await user.save()
-    
+
     return token;
 }
 
@@ -84,6 +101,13 @@ userSchema.pre('save', async function (next) {
         user.password = await bcrypt.hash(user.password, 8);
     }
 
+    next();
+})
+
+// Delete tasks for user when that user is removed / deleted
+userSchema.pre('remove', async function (next) {
+    const user = this;
+    await Task.deleteMany({ owner: user._id })
     next();
 })
 
