cd0ed93e67e35d462a4a5c9e4b48ec2a290c2044
Lindakat <lindakt16@gmail.com>
2019-08-01 23:21:54 -0500

    Added some event acknowledgement functionality; minor UI adjustments

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -1,12 +1,50 @@
 const socket = io();
 
+// HTML Elements - $ used to signify it's a DOM element (convention)
+const $messageForm = document.querySelector('#textForm');
+const $messageFormInput = $messageForm.querySelector('input');
+const $messageFormButton = $messageForm.querySelector('button');
+const $locationButton = document.querySelector('#sendLocation');
+
+
 socket.on('message', (welcome) => {
     console.log(welcome)
 })
 
-document.querySelector('#textForm').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    // disable form once submitted, prevent accidental resubmits
+    $messageFormButton.setAttribute('disabled', 'disabled');
     
     const message = e.target.elements.formMessage.value;
-    socket.emit('sendMessage', message);
+    socket.emit('sendMessage', message, (error) => {
+        // re-enable button click functionality, clear input, reset focus
+        $messageFormButton.removeAttribute('disabled');
+        $messageFormInput.value = '';
+        $messageFormInput.focus();
+
+        if (error) {
+            return console.log(error)
+        }
+        console.log('The message was delivered!')
+    });
+})
+
+$locationButton.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Gelocation is not supported by your browser.')
+    }
+
+    $locationButton.setAttribute('disabled', 'disabled');
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, () => {
+            console.log('Location shared!')
+            $locationButton.removeAttribute('disabled');
+        })
+    })
 })
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
+const Filter = require('bad-words');
 
 const app = express();
 const server = http.createServer(app);
@@ -18,8 +19,20 @@ io.on('connection', (socket) => {
     socket.emit('message', "Welcome!");
     socket.broadcast.emit('message', "A new user has joined!");
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message, cb) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return cb('No profanity!')
+        }
+
         io.emit('message', message);
+        cb()
+    })
+
+    socket.on('sendLocation', (position, cb) => {
+        io.emit('message', `https://google.com/maps?q=${position.latitude},${position.longitude}`)
+        cb()
     })
 
     socket.on('disconnect', () => {
